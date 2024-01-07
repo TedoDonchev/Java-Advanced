bac30eb779eec7b5564b615df69cb8330f278729
TedoDonchev <37153281+TedoDonchev@users.noreply.github.com>
2024-01-07 16:19:58 +0200

    Create _01_FillTheMatrix.java

---
+++ src/com/company/MultidimensionalArrays/_01_FillTheMatrix.java
@@ -0,0 +1,11 @@
+package com.company.MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class _01_FillTheMatrix {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+
+}
